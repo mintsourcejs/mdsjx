36eb18d79e7d37e00574c96b4ef9b5481dde4798
Ian Henshaw <ian@thehenshaws.net>
2023-12-24 09:34:27 +0000

    relocated building to code scope into useMdxScope hook

--- packages/mdxjs-react/src/useMdxScope.js
+++ packages/mdxjs-react/src/useMdxScope.js
@@ -1,7 +1,15 @@
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { MdxScopeContext } from "./MdxScopeContext";
 
-export function useMdxScope() {
-    const scope = useContext(MdxScopeContext);
+export function useMdxScope(scopeMapping) {
+    const globalScope = useContext(MdxScopeContext);
+
+    const scope = useMemo(() => {
+        return Object.entries(scopeMapping).reduce((result, [localName, alias]) => {
+            result[localName] = globalScope[alias];
+            return result;
+        }, {});
+    }, [globalScope, scopeMapping])
+
     return scope;
 }
